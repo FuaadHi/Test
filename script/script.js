626e1f2b601e0c5d487e8c38b72bbc537a167702
Fuaad <hydfwaad@gmail.com>
2021-10-02 09:18:33 +0200

    upload files

---
+++ script/script.js
@@ -0,0 +1,29 @@
+var toggler = document.querySelector(".toggler"),
+    list = document.querySelector(".links ul"),
+    themer = document.querySelector(".theme i"),
+    body = document.querySelector("body"),
+    top_arrow = document.querySelector(".fa-arrow-left");
+document.addEventListener("scroll", () => {
+    if (scrollY > 0) {
+        top_arrow.style.display = "block"
+    }
+    else top_arrow.style.display = "none"
+})
+top_arrow.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0
+})
+document.onclick = (e) => {
+    if (!e.target.classList.contains("toggler")) {
+        if (list.classList.contains("active")) {
+            list.classList.remove("active");
+        }
+    }
+}
+toggler.addEventListener("click", () => {
+    list.classList.toggle("active");
+})
+themer.addEventListener("click", () => {
+    body.classList.toggle("theme");
+    themer.classList.toggle("fa-moon")
+})
